2ec2f562e335388fd5181d04b0a8f321a3c76961
Yousuke Nakano <yousuke.computer@gmail.com>
2019-01-30 02:29:19 +0000

    ひらがなからローマ字に変換するときに使用する配列を定義，アルファベットからモールス信号に変換するときに使用する配列を定義している部分を見やすいように変更

--- morse_code.js
+++ morse_code.js
@@ -1,81 +1,69 @@
-(function(){
+(function () {
   'use strict';
 
   const inputTextBox = document.getElementById('input_text');
   const encodeButton = document.getElementById('encode_button');
 
-  function chengeJA_Alpha(char, assimilatedSound) {
+  function chengeJA_Alpha(str) {
     let result = '';
-    
+
     //TODO 引数charが全角カタカナだった場合ひらがなに変換する処理を実装
     //TODO 引数charが半角カタカナだった場合ひらがなに変換する処理を実装
-
     //TODO 引数をアルファベットに変換する処理を実装
     //TODO? めんどくさそうなのでやるかわからないが，ヘボン式にする処理を実装するかも?
-  
-    if('あ' <= char && char <= 'お'){  
-    } else if ('か' <= char && char <= 'こ') {
-      result += 'k';
-    }
 
-    /*
-    console.log((char.charCodeAt() + 1) / 2 % 5 / 1);
-    console.log('あ'.charCodeAt() / 2 % 5 / 1);
-    console.log(char.charCodeAt());
-    console.log('あ'.charCodeAt());
-    */
-    //ひらがなのチャーコードめんどくさい
-    console.log((char.charCodeAt()) / 2 % 5 / 1 === 'あ'.charCodeAt() / 2 % 5 / 1);
-    if(char / 2 % 5 === 'あ' / 2 % 5) {
-      result += 'a';
-    } else if (char / 2 % 5 === 'い' / 2 % 5) {
-      result += 'i';
-    } else if (char / 2 % 5 === 'う' / 2 % 5) {
-      result += 'u';
-    } else if (char / 2 % 5 === 'え' / 2 % 5) {
-      result += 'e';
-    } else if (char / 2 % 5 === 'お' / 2 % 5) {
-      result += 'o';
-    }
-    //console.log(result);
+    const roman_alphabet = [
+      //ぁあぃいぅうぇえぉお
+      'xa', 'a', 'xi', 'i', 'xu', 'u', 'xe', 'e', 'xo', 'o',
+      //かがきぎくぐけげこご
+     'ka', 'ga', 'ki', 'gi', 'ku', 'gu', 'ke', 'ge', 'ko', 'go',
+      //さざしじすずせぜそぞ
+      'sa', 'za', 'shi', 'ji', 'su', 'zu', 'se', 'ze', 'so', 'zo',
+      //ただちぢっつづてでとど
+      'ta', 'da', 'chi', 'ji', 'tt', 'tsu', 'zu', 'te', 'de', 'to', 'do',
+      //なにぬねの
+      'na', 'ni', 'nu', 'ne', 'no',
+      //はばぱひびぴふぶぷへべぺほぼぽ
+      'ha', 'ba', 'pa', 'hi', 'bi', 'pi', 'fu', 'bu', 'pu', 'he', 'be', 'pe', 'ho', 'bo', 'po',
+      //まみむめも
+    'ma', 'mi', 'mu', 'me', 'mo',
+      //ゃやゅゆょよ
+      'xya', 'ya', 'xyu', 'yu', 'xyo', 'yo',
+      //らりるれろ
+      'ra', 'ri', 'ru', 're', 'ro',
+      //ゎわゐゑを
+      'xwa', 'wa', 'i', 'e', 'o',
+      //んゔゕゖ
+      'n', 'vu', 'xka', 'xke'
+    ]
+
+
+
+
+
+
 
     return result;
   }
-  
+
   function chengeAlpha_Morse(str) {
     const morse_code = [
-      "・－", 
-      "－・・・", 
-      "－・－・", 
-      "－・・", 
-      "・", 
-      "・・－・", 
-      "－－・", 
-      "・・・・", 
-      "・・", 
-      "・－－－", 
-      "－・－", 
-      "・－・・", 
-      "－－", 
-      "－・", 
-      "－－－", 
-      "・－－・", 
-      "－－・－", 
-      "－・", 
-      "・・・", 
-      "－", 
-      "・・－", 
-      "・・・－", 
-      "・－－", 
-      "－・・－", 
-      "－－・・", 
-      "－・－－", 
+      //ABCDEFG
+      "・－", "－・・・", "－・－・", "－・・", "・", "・・－・", "－－・",
+      //HIJKLMN
+      "・・・・", "・・", "・－－－", "－・－", "・－・・", "－－", "－・",
+      //OPQRSTU
+      "－－－", "・－－・", "－－・－", "－・", "・・・", "－", "・・－",
+      //VWXYZ
+      "・・・－", "・－－", "－・・－", "－－・・", "－・－－"
     ]
+
+    
     let result = '';
     let i;
-    for(i = 0; i < str.length; i++) {
+    for (i = 0; i < str.length; i++) {
       let charCodeNum = str.charCodeAt(i);
-      if(charCodeNum >='a'.charCodeAt()) {
+      if (charCodeNum >= 'a'.charCodeAt()) {
         charCodeNum -= 'a'.charCodeAt() - 'A'.charCodeAt();
       }
       result += morse_code[charCodeNum - 'A'.charCodeAt()];
@@ -91,12 +79,12 @@
     let chengeCharJA_alpha;
 
     let i;
-    for(i = 0; i < inputText.length; i++) {
+    for (i = 0; i < inputText.length; i++) {
       //chengeJA_Alpha(inputText.charAt(i), false)
       //console.log(chengeJA_Alpha(inputText.charAt(i), false));
     }
     console.log(chengeAlpha_Morse(inputText));
-    
+
 
     console.log(inputTextBox.value);
   }
