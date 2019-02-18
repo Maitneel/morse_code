ec2895f3e44bce42b468486c80f1f3110c81633f
Yousuke Nakano <yousuke.computer@gmail.com>
2019-02-18 23:56:00 +0000

    ボタンを押された時にinputに入っていた文字をconsole.logで出力する処理を実装

--- decode/decode.js
+++ decode/decode.js
@@ -1,4 +1,11 @@
 (function () {
   'use strict';
 
+  const inputTextBox = document.getElementById('input_text');
+  const decodeButton = document.getElementById('decode');
+
+  decodeButton.onclick = () => {
+    let inputText = inputTextBox.value;
+    console.log(inputText);
+  }
 })();
